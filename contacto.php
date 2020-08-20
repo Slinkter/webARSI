ec618049178b88334846e6102b851f1a2d5032d4
Slinkter <luis.j.cueva@gmail.com>
2020-08-20 16:24:21 -0500

    se mejor el codigo de contacto.php

--- contacto.php
+++ contacto.php
@@ -1,24 +1,38 @@
 <?php
 
-$para = "ro_alex@consultingarsi.com";
-$titulo = 'Correo de Pagina Web Consulting ARSI';
+//$para = "ro_alex@consultingarsi.com";
 
-$form_name = $_POST["name"];
-$form_email = $_POST["email"];
-$form_tel = $_POST["phone"];
-$form_catetegory = $_POST["category"];
-$form_msj = $_POST["message"];
 
-$mensaje   = 'Nombre : '.$form_name  ."\n".
-             'Correo : '.$form_email."\n".   
-             'Telefono : '.$form_tel."\n". 
-             'Categoria : '.$form_catetegory."\n". 
-             'Mensaje : '.$form_msj."\n" ;
+if(isset($_POST["name"])){
 
-$cabeceras = 'From: webmaster@consultingarsi.com' . "\r\n" .
-    'Reply-To: webmaster@consultingarsi.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+    $form_name = $_POST["name"];
+    $form_email = $_POST["email"] ;
+    $form_tel = $_POST["phone"];
+    $form_catetegory = $_POST["category"];
+    $form_msj = $_POST["message"] ;
 
-mail($para, $titulo, $mensaje, $cabeceras);
-header("Location: index.html");
+    $to = "consultingarsiapp@gmail.com";
+    $titulo = 'Correo de Página Web Consulting ARSI';
+
+    $cabeceras = 'From: webmaster@consultingarsi.com' . "\r\n" .
+        'Reply-To: webmaster@consultingarsi.com' . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+    $mensaje   = 'Nombre : '.$form_name  ."\n".
+                'Correo : '.$form_email."\n".   
+                'Telefono : '.$form_tel."\n". 
+                'Categoria : '.$form_catetegory."\n". 
+                'Mensaje : '.$form_msj."\n" ;
+
+
+
+    mail($to, $titulo, $mensaje, $cabeceras);
+    header("Location: index.html");
+
+}
+
+
+
+
+
 ?>
